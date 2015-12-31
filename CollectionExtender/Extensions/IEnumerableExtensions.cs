1bcdc73031594ec54c3448b5f7b612a879dfa0fc
David Desmaisons <desmaisons_david@hotmail.com>
2015-12-30 23:44:22 -0200

    First Commit

---
+++ CollectionExtender/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionExtender.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+
+        [DebuggerStepThrough]
+        static public IEnumerable<T> ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            if (enumerable == null)
+                return null;
+
+            foreach (T o in enumerable)
+            {
+                action(o);
+            }
+
+            return enumerable;
+        }
+    }
+}
---
+++ CollectionExtenderTest/Extensions/IEnumerableExtensionsTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CollectionExtender.Extensions;
+using Xunit;
+using FluentAssertions;
+using NSubstitute;
+using Xunit.Extensions;
+
+namespace CollectionExtenderTest.Extensions
+{
+    public class IEnumerableExtensionsTest
+    {
+        private Action<int> _Action;
+        public IEnumerableExtensionsTest()
+        {
+            _Action = Substitute.For<Action<int>>();
+        }
+
+        [Fact]
+        public void ForEach_CalledOnNull_ReturnNull()
+        {
+            IEnumerable<int> enumerable = null;
+            var res = enumerable.ForEach(_Action);
+            res.Should().BeNull();
+            _Action.DidNotReceive().Invoke(Arg.Any<int>());
+        }
+
+        [Fact]
+        public void ForEach_Call_Action_On_All_Elements()
+        {
+            IEnumerable<int> enumerable = Enumerable.Range(0,10);
+            var res = enumerable.ForEach(_Action);
+            Received.InOrder(() =>
+            {
+                foreach(var num in enumerable)
+                {
+                    _Action(num);
+                }
+            });
+        }
+
+        [Theory, PropertyData("Datas")]
+        public void ForEach_Returns_CallingElement(IEnumerable<int> enumerable)
+        {
+            var res = enumerable.ForEach(_Action);
+            res.Should().Equal(enumerable);
+        }
+
+        public static IEnumerable<object[]> Datas
+        {
+            get
+            {
+                IEnumerable<int> Null = null;
+                yield return new[] { Null };
+                yield return new []{Enumerable.Empty<int>()};
+            }
+        }
+    }
+}

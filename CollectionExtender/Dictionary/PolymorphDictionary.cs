dfee343de4b9d5672644b601ed03f8f4c75a0aca
David Desmaisons <desmaisons_david@hotmail.com>
2016-01-06 23:47:36 -0200

    clean-up

--- CollectionExtender/Dictionary/PolymorphDictionary.cs
+++ CollectionExtender/Dictionary/PolymorphDictionary.cs
@@ -9,24 +9,17 @@ namespace CollectionExtender.Dictionary
 {
     public class PolymorphDictionary<TKey, TValue> : IDictionary<TKey, TValue> where TKey : class 
     {
-        //private IDictionary<TKey, TValue> _Implementation;
         private IMutableDictionary<TKey, TValue> _Implementation;
-        //private IDictionaryLifeCycleStrategy<TKey, TValue> _LifeCycleStrategy;
 
         public PolymorphDictionary(int TransitionToDictionary = 25)
         {
             _Implementation = new MutableSingleDictionary<TKey, TValue, MutableListDictionary<TKey, TValue>>();
             bool comparable = typeof(TKey).GetInterfaces().Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IComparable<>) && (i.GetGenericArguments()[0]) == typeof(TKey)).Any();
-           
-            //_LifeCycleStrategy = (comparable) ?
-            //        (IDictionaryLifeCycleStrategy<TKey, TValue>) new SortedDictionaryLifeCycleStrategy<TKey, TValue>(TransitionToDictionary) :
-            //        (IDictionaryLifeCycleStrategy<TKey, TValue>) new ListDictionaryLifeCycleStrategy<TKey, TValue>(TransitionToDictionary);
         }
 
         public void Add(TKey key, TValue value)
         {
             _Implementation = _Implementation.Add(key, value);
-            //_LifeCycleStrategy.Add(ref _Implementation, key, value);
         }
 
         public void Add(KeyValuePair<TKey, TValue> item)
@@ -41,9 +34,6 @@ namespace CollectionExtender.Dictionary
 
         public bool Remove(KeyValuePair<TKey, TValue> item)
         {
-            //if (_Implementation == null)
-            //    return false;
-
             TValue tv = default(TValue);
 
             if (!_Implementation.TryGetValue(item.Key, out tv))
@@ -60,56 +50,36 @@ namespace CollectionExtender.Dictionary
             bool res = false;
             _Implementation = _Implementation.Remove(key, out res);
             return res;
-            //return _LifeCycleStrategy.Remove(ref _Implementation, key);
         }
 
         public bool ContainsKey(TKey key)
         {
-            //return (_Implementation == null) ? false : _Implementation.ContainsKey(key);
             return  _Implementation.ContainsKey(key);
         }
 
         public ICollection<TKey> Keys
         {
-            //get { return (_Implementation == null) ? new List<TKey>() : _Implementation.Keys; }
             get { return _Implementation.Keys; }
         }
 
         public bool TryGetValue(TKey key, out TValue value)
         {
-            //if (_Implementation == null)
-            //{
-            //    value = default(TValue);
-            //    return false;
-            //}
-
             return _Implementation.TryGetValue(key, out value);
         }
 
         public ICollection<TValue> Values
         {
-            //get { return (_Implementation == null) ? new List<TValue>() : _Implementation.Values; }
             get { return _Implementation.Values; }
         }
 
         public TValue this[TKey key]
         {
-            get
-            {
-                //if (_Implementation == null)
-                //    throw new KeyNotFoundException();
-                return _Implementation[key];
-            }
-            set
-            {
-                //_LifeCycleStrategy.Update(ref _Implementation, key, value);
-                _Implementation = _Implementation.Update(key, value);
-            }
+            get { return _Implementation[key]; }
+            set { _Implementation = _Implementation.Update(key, value); }
         }
 
         public bool Contains(KeyValuePair<TKey, TValue> item)
         {
-            //return (_Implementation == null) ? false : _Implementation.Contains(item);
             return _Implementation.Contains(item);
         }
 
@@ -120,9 +90,6 @@ namespace CollectionExtender.Dictionary
 
             if (arrayIndex < 0)
                 throw new ArgumentOutOfRangeException();
-
-            //if (_Implementation == null)
-            //    return;
 
             _Implementation.CopyTo(array, arrayIndex);
         }

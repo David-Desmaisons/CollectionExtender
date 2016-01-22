2795599c380035128844005d9a27cd3f297993dd
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2016-01-22 12:59:31 -0200

    Clean-Up 2

--- MoreCollection/Dictionary/Internal/DictionarySwitcher.cs
+++ MoreCollection/Dictionary/Internal/DictionarySwitcher.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MoreCollection.Dictionary.Internal
+﻿namespace MoreCollection.Dictionary.Internal
 {
     internal class DictionarySwitcher<TKey, TValue>  where TKey : class
     {

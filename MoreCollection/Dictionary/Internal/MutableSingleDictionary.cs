9738220ce731791e5a496b9544ac56fccf94e415
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2016-01-26 12:38:59 -0200

    Clean-up using and not used classes

--- MoreCollection/Dictionary/Internal/MutableSingleDictionary.cs
+++ MoreCollection/Dictionary/Internal/MutableSingleDictionary.cs
@@ -1,9 +1,5 @@
 ﻿using MoreCollection.Dictionary.Internal.Strategy;
-using MoreCollection.Infra;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace MoreCollection.Dictionary.Internal
 {

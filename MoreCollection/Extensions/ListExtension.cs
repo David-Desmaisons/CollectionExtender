e7ed9631c57409e572af21fb2bf04de53574b335
David Desmaisons <desmaisons_david@hotmail.com>
2016-02-13 16:35:26 -0200

    Add listextension move

--- MoreCollection/Extensions/ListExtension.cs
+++ MoreCollection/Extensions/ListExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace MoreCollection.Extensions
 {
@@ -8,9 +9,34 @@ namespace MoreCollection.Extensions
         public static IList<T> AddRange<T>(this IList<T> list, IEnumerable<T> enumerable)
         {
             if (list == null)
-                throw new ArgumentNullException("enumerable");
+                throw new ArgumentNullException("list");
 
             list.ForEach(list.Add);
+            return list;
+        }
+
+        public static IList<T> Move<T>(this IList<T> list, int oldIndex, int newIndex)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+
+            var observable = list as ObservableCollection<T>;
+            if (observable != null)
+            {
+                observable.Move(oldIndex,newIndex);
+                return list;
+            }
+
+            var count = list.Count;
+            if ((oldIndex < 0) || (oldIndex > count - 1))
+                throw new ArgumentOutOfRangeException("oldIndex");
+
+            if ((newIndex < 0) || (newIndex > count - 1))
+                throw new ArgumentOutOfRangeException("newIndex");
+
+            var item = list[oldIndex];
+            list.RemoveAt(oldIndex);
+            list.Insert(newIndex, item);
             return list;
         }
     }
--- MoreCollectionTest/Extensions/ListExtensionTest.cs
+++ MoreCollectionTest/Extensions/ListExtensionTest.cs
@@ -5,16 +5,25 @@ using MoreCollection.Extensions;
 using Xunit;
 using FluentAssertions;
 using Xunit.Extensions;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace MoreCollectionTest.Extensions
 {
     public class ListExtensionTest
     {
         private IList<int> List { get { return _List; } }
-        private readonly List<int> _List;
+        private readonly IList<int> _List;
+        private readonly IList<int> _FullList;
+        private readonly IList<int> _FullListObservable;
+        private readonly ObservableCollection<int> _RawFullListObservable;
+        private readonly IList<int> _NullList;
         public ListExtensionTest()
         {
             _List = new List<int>();
+            _FullList = new List<int>( new[]{0, 1, 2});
+            _RawFullListObservable = new ObservableCollection<int>(new[] { 0, 1, 2 });
+            _FullListObservable = _RawFullListObservable;
         }
 
         [Theory, PropertyData("Data")]
@@ -31,7 +40,7 @@ namespace MoreCollectionTest.Extensions
         public void AddRange_ReturnsCallingList(IEnumerable<int> enumerable)
         {
             var res = List.AddRange(enumerable);
-            res.Should().Equals(List);
+            res.Should().BeSameAs(List);
         }
 
         public static IEnumerable<object[]> Data
@@ -45,12 +54,72 @@ namespace MoreCollectionTest.Extensions
             }
         }
 
+
         [Fact]
         public void Addrange_CalledOnNull_ThrowException()
         {
-            IList<int> list = null;
-            Action Do = () => list.AddRange(Enumerable.Empty<int>() );
+            Action Do = () => _NullList.AddRange(Enumerable.Empty<int>());
             Do.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Move_CalledOnNull_ThrowException()
+        {
+            Action Do = () => _NullList.Move(0, 27);
+            Do.ShouldThrow<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Move_CalledWithWrongIndex_ThrowArgumentException()
+        {
+            Action Do = () => _FullList.Move(-1, 1);
+            Do.ShouldThrow<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void Move_CalledWithWrongIndex_2_ThrowArgumentException()
+        {
+            Action Do = () => _FullList.Move(10, 1);
+            Do.ShouldThrow<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void Move_CalledWithWrongIndex_3_ThrowArgumentException()
+        {
+            Action Do = () => _FullList.Move(0, -1);
+            Do.ShouldThrow<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void Move_CalledWithWrongIndex_4_ThrowArgumentException()
+        {
+            Action Do = () => _FullList.Move(0, 3);
+            Do.ShouldThrow<ArgumentOutOfRangeException>();
+        }
+
+        [Fact]
+        public void Move_Behave_AsExpected()
+        {
+            _FullList.Move(0, 2);
+            _FullList.Should().Equal(1,2,0);
+        }
+
+        [Fact]
+        public void Move_Behave_AsExpected_2()
+        {
+            _FullList.Move(2, 0);
+            _FullList.Should().Equal(2, 0, 1);
+        }
+
+        [Fact]
+        public void Move_CallObservableCollectionMove()
+        {
+            _RawFullListObservable.MonitorEvents();
+            var res = _FullListObservable.Move(2, 0);
+            res.Should().BeSameAs(_FullListObservable);
+            _FullListObservable.Should().Equal(2, 0, 1);
+            _RawFullListObservable.ShouldRaise("CollectionChanged")
+                .WithArgs<NotifyCollectionChangedEventArgs>(args => args.Action == NotifyCollectionChangedAction.Move);
         }
     }
 }

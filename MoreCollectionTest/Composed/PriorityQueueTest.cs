e24283da2798747412796b3510e66044ec6d7d74
desmaisons_david <desmaisons_david@hotmail.com>
2016-06-09 22:09:53 -0300

    Add algo extender

--- MoreCollectionTest/Composed/PriorityQueueTest.cs
+++ MoreCollectionTest/Composed/PriorityQueueTest.cs
@@ -298,22 +298,5 @@ namespace MoreCollectionTest.Composed
             mio.TryGetTarget(out res).Should().BeFalse();
             res.Should().BeNull();
         }
-
-        private class MyObject : IComparable<MyObject>
-        {
-            public MyObject(string v1, int v2) 
-            {
-                Name = v1;
-                Value = v2;
-            }
-
-            public int Value { get; set; }
-
-            public string Name { get; set; }
-            public int CompareTo(MyObject other) 
-            {
-                return Name.CompareTo(other.Name);
-            }
-        }
     }
 }
---
+++ MoreCollectionTest/MyObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MoreCollectionTest
+{
+    internal class MyObject : IComparable<MyObject>
+    {
+        public MyObject(string v1, int v2)
+        {
+            Name = v1;
+            Value = v2;
+        }
+
+        public int Value { get; set; }
+
+        public string Name { get; set; }
+        public int CompareTo(MyObject other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+    }
+}

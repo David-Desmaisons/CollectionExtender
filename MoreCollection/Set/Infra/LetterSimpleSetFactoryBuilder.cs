fa356e18e98899cd320f70852a3762b49e3f4067
desmaisons_david <desmaisons_david@hotmail.com>
2017-02-11 16:27:04 -0200

    Making set factory none generic and static

--- MoreCollection/Set/Infra/LetterSimpleSetFactoryBuilder.cs
+++ MoreCollection/Set/Infra/LetterSimpleSetFactoryBuilder.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Concurrent;
-
-namespace MoreCollection.Set.Infra
+﻿namespace MoreCollection.Set.Infra
 {
     internal class LetterSimpleSetFactoryBuilder
     {
-        private static readonly ConcurrentDictionary<int, LetterSimpleSetFactory> _Factory = new ConcurrentDictionary<int, LetterSimpleSetFactory>();
-        internal static Func<int, ILetterSimpleSetFactory> GetFactory { get; set; } =  (maxList) => _Factory.GetOrAdd(maxList, _ => new LetterSimpleSetFactory(maxList));
+        private const int _Switch = 10;
+
+        internal static ILetterSimpleSetFactory Factory { get; set; } = new LetterSimpleSetFactory(_Switch);
     }
 }
--- MoreCollectionTest/Set/HybridSetIntegratedTest.cs
+++ MoreCollectionTest/Set/HybridSetIntegratedTest.cs
@@ -1,10 +1,12 @@
 ﻿using MoreCollection.Set;
+using Xunit;
 
 namespace MoreCollectionTest.Set
 {
+    [Collection("Changing Default static stategy")]
     public class HybridSetIntegratedTest : SetTest
     {
-        public HybridSetIntegratedTest(): base(new HybridSet<string>(3))
+        public HybridSetIntegratedTest(): base(new HybridSet<string>())
         {
         }
     }

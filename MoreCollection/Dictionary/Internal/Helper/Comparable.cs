0f945e723ec1b1323ea1b62cb1f85d112cc5d662
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2017-08-10 11:58:46 -0300

    Adding dictionary factory

---
+++ MoreCollection/Dictionary/Internal/Helper/Comparable.cs
@@ -0,0 +1,26 @@
+﻿using MoreCollection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MoreCollection.Dictionary.Internal.Helper
+{
+    internal static class Comparable 
+    {
+        private static readonly IDictionary<Type, bool> _Comparable = new Dictionary<Type, bool>();
+
+        private static readonly Type IComparableType = typeof(IComparable<>);
+
+        private static bool PrivateIsComparable(Type type) 
+        {
+            return type.GetInterfaces().Any(interfaceType => interfaceType.IsGenericType
+                            && interfaceType.GetGenericTypeDefinition() == IComparableType
+                            && (interfaceType.GetGenericArguments()[0]) == type);
+        }
+
+        internal static bool IsComparable(this Type type) 
+        {
+            return _Comparable.GetOrAddEntity(type, PrivateIsComparable);
+        }
+    }
+}
---
+++ MoreCollectionTest/Dictionary/Internal/Helper/ComparableTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using MoreCollection.Dictionary.Internal.Helper;
+using System;
+using Xunit;
+
+namespace MoreCollectionTest.Dictionary.Internal.Helper 
+{
+    public class ComparableTest 
+    {
+        [Theory]
+        [InlineData(typeof(bool), true)]
+        [InlineData(typeof(string), true)]
+        [InlineData(typeof(decimal), true)]
+        [InlineData(typeof(object), false)]
+        [InlineData(typeof(ComparableTest), false)]
+        public void IsComparable_returns_correct_value(Type type, bool expectedIsComparable)
+        {
+            type.IsComparable().Should().Be(expectedIsComparable);
+        }
+    }
+}
